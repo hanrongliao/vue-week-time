8045013ea839977246eba63e7aa2db5b107a5a50
hanrong <hanrongliao@gmail.com>
2019-05-06 23:14:26 +0800

    update dev dependence

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 var path = require('path')
 var webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+
+
 
 const entry = process.env.NODE_ENV === 'production' ? './src/index.js' : './src/main.js';
 
@@ -33,11 +37,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.vue$/,
         use: [
-          'vue-style-loader',
-          'css-loader'
+          'vue-loader',
         ],
+        exclude: '/node_modules/'
       },
       {
         test: /\.scss$/,
@@ -48,49 +52,13 @@ module.exports = {
         ],
       },
       {
-        test: /\.sass$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader?indentedSyntax'
-        ],
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader'
-            ],
-            'sass': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader?indentedSyntax'
-            ]
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
       },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
     ]
   },
+  plugins: [new VueLoaderPlugin()],
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
@@ -98,9 +66,6 @@ module.exports = {
     extensions: ['*', '.js', '.vue', '.json']
   },
   devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true
   },
   performance: {
     hints: false
